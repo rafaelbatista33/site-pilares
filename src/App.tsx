b3eefc795ee0e786576720c9764907661e65823f
Rodrigobs4 <rodrigobs4@gmail.com>
2026-04-26 14:47:18 -0300

    disable public chat widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 // src/App.tsx
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleIndex1Page from "./pages/home/SingleIndex1Page";
 import ContactPage from "./pages/contact/ContactPage";
 import BusinessPage from "./pages/business/BusinessPage";
 import AdminPage from "./pages/admin/AdminPage";
-import ChatWidget from "./components/ChatWidget";
 
 const AppRoutes: React.FC = () => {
-  const location = useLocation();
-  const isAdminPage = location.pathname.startsWith("/admin");
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<SingleIndex1Page />} />
-        <Route path="/contato" element={<ContactPage />} />
-        <Route path="/business" element={<BusinessPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-      {!isAdminPage && <ChatWidget />}
-    </>
+    <Routes>
+      <Route path="/" element={<SingleIndex1Page />} />
+      <Route path="/contato" element={<ContactPage />} />
+      <Route path="/business" element={<BusinessPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   );
 };
 
